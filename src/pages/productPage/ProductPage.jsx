ccde13c4911c44b6a16a4c0c7afe07f920a66a7f
DevFranceca <dynamicfranchescer@gmail.com>
2024-09-25 21:29:01 +0100

    media query

---
+++ src/pages/productPage/ProductPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductPage.css'
+
+const ProductPage = () => {
+  return (
+    <div>
+      All Products page
+    </div>
+  )
+}
+
+export default ProductPage
